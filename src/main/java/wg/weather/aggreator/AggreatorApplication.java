2d2ec1263b3e50f7080f137cb093eacc2f7d3538
Gałecki <wojciechga@yrthak.home>
2019-05-12 16:17:59 +0200

    initial commit

---
+++ src/main/java/wg/weather/aggreator/AggreatorApplication.java
@@ -0,0 +1,13 @@
+package wg.weather.aggreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wg/weather/aggreator/AggreatorApplicationTests.java
@@ -0,0 +1,16 @@
+package wg.weather.aggreator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AggreatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
